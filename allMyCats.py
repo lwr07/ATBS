6cd022b9aeb2aa18e11cd0cece49ccc56d354c67
lwr07 <wayne.richter@gmail.com>
2016-09-11 14:11:02 -0500

    testing IDE commit

--- allMyCats.py
+++ allMyCats.py
@@ -1,11 +1,12 @@
 catNames = []
 while True:
-	print('Enter the name of cat ' + str(len(catNames) + 1) + ' (or enter nothing to stop.):')
-	name = input()
-	if name == '':
-		break
-	catNames = catNames + [name] #list concatenation
+    print('Enter the name of cat {0} (or enter nothing to stop.):'.format(str(len(catNames) + 1)))
+    name = input()
+    if name == '':
+        break
+    catNames = catNames + [name]  # list concatenation
 print('The cat names are:')
 for name in catNames:
-	print('  ' + name)
-#This a github test comment
+    print('  ' + name)
+    # This a github test comment
+    # This is a second github test
